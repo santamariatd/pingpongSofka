7e6d0be5a34cb3736f3e89d241c089c4bce6f03a
stdan.262 <danisantamaria97@gmail.com>
2021-11-19 03:14:50 -0500

    Agregada la funcion Draw y Bar con su prototipo

--- main.js
+++ main.js
@@ -11,20 +11,63 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
 (function(){
+    self.Bar = function (x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this,board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function (){
+
+        },
+        up: function (){
+
+        }
+    }
+})();
+
+(function(){
     self.BoardView = function(canvas,board){
+
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
         this.board = board;
         this.ctx = canvas.getContext("2d");
     } 
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for (var i= this.board.elements.length-1; i>=0; i--){
+                var el = this.board.elements[i];
+
+                draw(this.ctx,el);
+            };
+        }
+    }
+
+    function draw(ctx,element){
+       if (element !== null && element.hasOwnProperty("kind")){
+        switch(element.kind){
+            case "retangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+        }
+       }
+      
+    }
 })();
 
 self.addEventListener("Load",main);
@@ -32,5 +75,10 @@ self.addEventListener("Load",main);
 function main (){
     var board = new Board(800,400);
     var canvas = document.getElementsById("canvas");
+    var bar = new Bar(20,100,40,100,board);
+    var bar = new Bar(735,100,40,100,board);
     var board_view = new BoardView(canvas,board);
+
+
+    board_view_draw();
 }
