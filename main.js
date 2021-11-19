e38e5036bbcbf1e918fbe115870a6869290be8a6
stdan.262 <danisantamaria97@gmail.com>
2021-11-19 03:50:14 -0500

    Se agrega evento para saber cual tecla presiona el usuario

--- main.js
+++ main.js
@@ -26,14 +26,18 @@
         this,board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function (){
-
+            this.y += this.speed;
         },
         up: function (){
-
+            this.y -= this.speed;
+        },
+        toString: function (){
+            return "x: " + this.x + " y: " + this.y;
         }
     }
 })();
@@ -70,15 +74,28 @@
     }
 })();
 
+
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar = new Bar(735,100,40,100,board);
+var canvas = document.getElementsById("canvas");
+var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown",function(ev){
+    console.log(ev.keyCode);
+    if (ev.keyCode == 38){
+        bar.up();
+    }
+    else if (ev.keyCode == 40){
+        bar.down();
+    }
+
+    
+});
+
 self.addEventListener("Load",main);
 
 function main (){
-    var board = new Board(800,400);
-    var canvas = document.getElementsById("canvas");
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(735,100,40,100,board);
-    var board_view = new BoardView(canvas,board);
 
-
-    board_view_draw();
+    board_view.draw();
 }
