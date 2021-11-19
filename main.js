077ea6f46dae136f0bf438bd334cf0c25f6dd982
stdan.262 <danisantamaria97@gmail.com>
2021-11-19 05:47:53 -0500

    Se agrega movimiento a la pelota y se agrega el evento que detecta cuando el usuario presiona la tecla espacio para pausar, continuar o comenzar juego

--- main.js
+++ main.js
@@ -6,11 +6,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements(){
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball);
             return elements;
         }
@@ -24,9 +25,18 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle";
+
+       
+    }
+    self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
     }
 })();
 (function(){
@@ -76,8 +86,12 @@
             };
         },
         play: function(){
-           this.clean();
-           this.draw();
+           if (this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+           }
+          
         }
     }
 
@@ -105,25 +119,34 @@ var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas,board);
 var ball = new Ball(350,100,10,board);
 
-//window.requestAnimationFrame(controller);
-
 document.addEventListener("keydown",function(ev){
-    ev.preventDefault();
+    
     if (ev.keyCode === 38){
+        ev.preventDefault();
         bar.up();
     }
     else if (ev.keyCode === 40){
+        ev.preventDefault();
         bar.down();
     } else if (ev.keyCode === 87){
+        ev.preventDefault();
         //W
         bar_2.up();
     } else if (ev.keyCode === 83){
+        ev.preventDefault();
         //S
         bar_2.down();
+    } else if(ev.keyCode === 32){
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 
 });
-
+board_view.draw();
+window.requestAnimationFrame(controller);
+/*setTimeout(function(){
+    ball.direction = -1;
+},4000);*/
 
 function controller (){
     board_view.play();
