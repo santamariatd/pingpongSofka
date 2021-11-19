e0943d1475ac40dc6c64f851f8626248da034861
stdan.262 <danisantamaria97@gmail.com>
2021-11-19 04:59:54 -0500

    Se agregan los controles del jugador 2, se cambia elnombre main por controller y se transladan las funciones clean y draw al BoardView, y se crea la funcion para dibujar la pelota.

--- main.js
+++ main.js
@@ -16,14 +16,26 @@
         }
     }
 })();
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
 
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
 (function(){
     self.Bar = function (x,y,width,height,board){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this,board = board;
+        this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
         this.speed = 10;
@@ -53,49 +65,67 @@
     } 
 
     self.BoardView.prototype = {
+        clean: function (){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
         draw: function(){
             for (var i= this.board.elements.length-1; i>=0; i--){
                 var el = this.board.elements[i];
 
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+           this.clean();
+           this.draw();
         }
     }
 
     function draw(ctx,element){
-       if (element !== null && element.hasOwnProperty("kind")){
         switch(element.kind){
-            case "retangle":
+            case "rectangle":
                 ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
-       }
-      
+       
     }
 })();
 
 
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100,board);
-var bar = new Bar(735,100,40,100,board);
-var canvas = document.getElementsById("canvas");
+var bar_2 = new Bar(735,100,40,100,board);
+var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas,board);
+var ball = new Ball(350,100,10,board);
+
+//window.requestAnimationFrame(controller);
 
 document.addEventListener("keydown",function(ev){
-    console.log(ev.keyCode);
-    if (ev.keyCode == 38){
+    ev.preventDefault();
+    if (ev.keyCode === 38){
         bar.up();
     }
-    else if (ev.keyCode == 40){
+    else if (ev.keyCode === 40){
         bar.down();
+    } else if (ev.keyCode === 87){
+        //W
+        bar_2.up();
+    } else if (ev.keyCode === 83){
+        //S
+        bar_2.down();
     }
 
-    
 });
 
-self.addEventListener("Load",main);
 
-function main (){
-
-    board_view.draw();
+function controller (){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
